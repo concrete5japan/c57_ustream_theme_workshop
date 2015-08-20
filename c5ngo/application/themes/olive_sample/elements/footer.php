31e2ad88c02d5c40a196cca68fefde6205c13743
Katz Ueno <katz@concrete5.co.jp>
2015-08-20 23:18:54 +0900

    Move the directory to answer

---
+++ c5ngo/application/themes/olive_sample/elements/footer.php
@@ -0,0 +1,35 @@
+<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
+	<div class="row">
+		<div class="twelve columns">
+			<p class="right scroll-page-top"><a href="#page">ページの先頭へ</a></p>
+		</div>
+	</div>
+
+	<footer id="pageFooter" class="row" role="contentinfo">
+		<div class="twelve columns">
+			<hr />
+			<div class="row">
+				<div class="seven columns">
+					<?php
+					$a = new GlobalArea('Footer');
+					$a->display();
+					?>
+				</div>
+				<div class="five columns">
+					<p class="copyright right"><small>Copyright &copy; <?php echo date('Y')?> <?php echo Config::get('concrete.site');?></small></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</div>
+</div>
+
+<?php  Loader::element('footer_required'); ?>
+
+<script src="<?php echo $this->getThemePath(); ?>/javascripts/jquery.foundation.forms.js"></script>
+<script src="<?php echo $this->getThemePath(); ?>/javascripts/jquery.foundation.orbit.js"></script>
+<script src="<?php echo $this->getThemePath(); ?>/main.js"></script>
+
+</body>
+</html>
